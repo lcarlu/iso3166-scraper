781d359c91aa414e449917d2045a7873796b0231
LudoCarlu <ludoviccarlu@gmail.com>
2026-02-02 22:08:15 +0100

    main to call selenium driver and create the file

---
+++ iso3166_scraper/src/main.py
@@ -0,0 +1,230 @@
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from seleniumbase import Driver
+import asyncio
+import pandas as pd
+from datetime import date, datetime
+from utils import async_measure_execution_time, save_file
+from classes import Country, CodeElement, CodeElementStatus
+from typing import List, Dict, Any
+from parser import *
+import sys
+from config.logger import get_logger
+import argparse
+import itertools
+
+def get_arguments():
+
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--language", type=str, default='en', choices=['en', 'fr'], help="choose the language for the content of all files. Choice between fr and en (default = en)")
+    parser.add_argument("--download", action="store_true", help="Download all html file before parsing")
+
+    return parser.parse_args()
+
+
+logger = get_logger(__name__)
+
+COUNTRIES_EXPECTED_COLUMNS = [
+    "alpha_2_code",
+    "alpha_3_code",
+    "alpha_4_code",
+    "numeric_code",
+    "short_name",
+    "short_name_lower_case",
+    "full_name",
+    "independent",
+    "territory_name",
+    "status",
+    "status_remark",
+    "remarks",
+    "remark_part_1",
+    "remark_part_2",
+    "remark_part_3"
+    ]
+
+COUNTRY_CODES_COLLECTION_EXCEPTED_COLUMNS = [
+    "alpha_2_code",
+    "short_name_lower_case",
+    "status",
+    "page_id"
+]
+
+SUBDIVISIONS_EXCEPTED_COLUMNS = [
+    "alpha_2_code",
+    "alpha_3_code",
+    "numeric_code",
+    "subdivision_category",
+    "subdivision_code",
+    "subdivision_name",
+    "local_variant",
+    "language_code",
+    "romanization_system",
+    "parent_subdivision_code"
+]
+
+def get_country_codes_collection_html(driver, url: str) -> str:
+
+    # open URL using UC mode with 6 second reconnect time to bypass initial detection
+    # Pass the cloudfare challenge
+    driver.uc_open_with_reconnect(url, reconnect_time=6)
+
+    # attempt to click the CAPTCHA checkbox if present
+    driver.uc_gui_click_captcha()
+    #driver.save_screenshot("cloudflare-challenge.png")
+
+    try:
+        # Accept cookies
+        wait = WebDriverWait(driver, 20)
+        wait.until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
+    
+    except Exception as expection:
+        logger.error(f"{expection=}")
+
+    # take a screenshot of the current page and save it
+    #driver.save_screenshot("cloudflare-challenge2.png")
+
+    # wait for the element to load
+    wait = WebDriverWait(driver, timeout=10)
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "grs-grid")))
+
+    # Get the html from the page
+    return driver.page_source
+
+def get_country_html(driver, url: str) -> str:
+
+    driver.get(url)
+    driver.refresh() # refresh the driver in order to change correctly the page source
+    logger.debug(f"{driver.current_url=}")
+    wait = WebDriverWait(driver, timeout=20)
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "core-view-summary")))
+    return driver.page_source
+
+def get_all_countries_subdivisions(countries: List[Country]) -> List[Dict[str, Any]]:
+    subdivisions = [country.get_subdivisions() for country in countries]
+    flattened_subdivisions = list(itertools.chain(*subdivisions))
+
+    return flattened_subdivisions
+
+def get_all_countries_languages(countries: List[Country]) -> List[Dict[str, Any]]:
+    languages = [country.get_languages() for country in countries]
+    flattened_languages = list(itertools.chain(*languages))
+    return flattened_languages
+
+
+def generate_csv(input: List[Dict[str, str]], file_path: Path, expected_columns: List[str]) -> None:
+
+    today = date.today()
+    df = pd.DataFrame.from_dict(input, dtype=str)
+    logger.info(df.head(10))
+    logger.info(df.shape)
+    logger.info(f'{expected_columns=}')
+    df = df[expected_columns]
+
+    file_path.parent.mkdir(exist_ok=True, parents=True)
+    df.to_csv(file_path, sep='|', index=False, encoding='utf-8')
+
+@measure_execution_time
+def main() -> None:
+
+    BASE_URL = "https://www.iso.org/obp/ui/"
+    COUNTRY_CODES_COLLECTION_PAGE_ID = "#iso:pub:PUB500001:en"
+    DATA_DIR: Path = Path.cwd() / "iso3166_scraper" / "data"
+    
+    # Read input arguments
+    arguments = get_arguments()
+    logger.info(f"{arguments=}")
+
+    country_codes_collection_url = f"{BASE_URL}{COUNTRY_CODES_COLLECTION_PAGE_ID}"
+
+    start_date: date = date.today()
+    formatted_start_date: str = start_date.strftime("%Y%m%d")
+    downloaded_files_dir: Path = DATA_DIR / formatted_start_date / "downloaded_files" / arguments.language
+    output_files_dir: Path = DATA_DIR / formatted_start_date / "output_files" / arguments.language
+
+    try:
+        driver = Driver(uc=True, headless=True)
+        country_codes_collection_html = get_country_codes_collection_html(driver, country_codes_collection_url)
+
+        # Save html content
+        if arguments.download:
+            save_file(downloaded_files_dir / f"country_codes_collection.html", country_codes_collection_html)
+
+        # Parse the decoding table
+        code_elements_statuses: List[CodeElementStatus] = parse_code_elements_statuses(country_codes_collection_html)
+        logger.info(f"{code_elements_statuses=}")
+
+        # Parse the country codes collection
+        country_codes_collection: List[CodeElement] = parse_country_codes_collection(country_codes_collection_html, code_elements_statuses)
+        logger.info(f"{country_codes_collection[:10]=}")
+
+        countries_html: List[str] = []
+        countries: List[Country] = []
+
+        # Change only the base url for the countries pages
+        # The main page is not translated
+        if arguments.language == "fr":
+            BASE_URL: str = f"{BASE_URL}fr/"
+
+        """
+        TODO : Optimize downloading with parallel execution
+        https://stackoverflow.com/questions/42732958/python-parallel-execution-with-selenium
+        """
+
+        for code_element in country_codes_collection:
+            page_id = code_element.page_id
+            file_name = f"{code_element.alpha_2_code}.html"
+
+            if page_id is not None:
+                url: str = f"{BASE_URL}{page_id}"
+                logger.debug(f"{url=}")
+                html: str = get_country_html(driver, url)
+                countries_html.append(html)
+
+                if arguments.download:
+                    save_file(downloaded_files_dir / file_name, html)
+
+        for html in countries_html:
+            country = parse_country(html, arguments.language)
+            countries.append(country)
+
+        logger.info(f"{countries=}")
+
+        countries_subdivisions = get_all_countries_subdivisions(countries)
+        logger.info(f"{countries_subdivisions=}")
+
+        # Generate csv files
+
+        countries_file_path = output_files_dir / "countries.csv"
+        country_codes_collection_file_path = output_files_dir / "country_codes_collection.csv"
+        subdivisions_file_path = output_files_dir / "subdivisions.csv"
+
+        generate_csv(
+            [c.to_dict() for c in countries],
+            countries_file_path,
+            COUNTRIES_EXPECTED_COLUMNS
+        )
+
+        generate_csv(
+            [c.to_dict() for c in country_codes_collection],
+            country_codes_collection_file_path,
+            COUNTRY_CODES_COLLECTION_EXCEPTED_COLUMNS
+        )
+
+        generate_csv(
+            countries_subdivisions,
+            subdivisions_file_path,
+            SUBDIVISIONS_EXCEPTED_COLUMNS
+        )
+
+    except Exception as e:
+        raise e
+    
+    finally:
+        # close the browser and end the session
+        driver.quit()
+                
+if __name__ == '__main__':
+    main()
--- scraper.py
+++ scraper.py
@@ -6,10 +6,37 @@ import pandas as pd
 from datetime import date, datetime
 import time
 import logging
+from unidecode import unidecode
 
 RENDER_SLEEP = 5
 BASE_URL = "https://www.iso.org/obp/ui/"
 COUNTRY_CODES_COLLECTION_PAGE_ID = "#iso:pub:PUB500001:en"
+
+COUNTRY_CODES_EXPECTED_COLUMNS = [
+    "alpha_2_code",
+    "alpha_3_code",
+    "alpha_4_code",
+    "numeric_code",
+    "short_name",
+    "short_name_lower_case",
+    "full_name",
+    "independent",
+    "territory_name",
+    "status",
+    "status_remark",
+    "remarks",
+    "remark_part_1",
+    "remark_part_2",
+    "remark_part_3"
+    ]
+
+CODE_ELEMENTS_STATUSES_EXPECTED_COLUMNS = ["status_code", "status_text", "status"]
+COUNTRY_CODES_COLLECTION_EXCEPTED_COLUMNS = [
+    "alpha_2_code",
+    "short_name_lower_case",
+    "status_code",
+    "page_id"
+]
 
 logging.basicConfig(
     format="%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s",
@@ -29,20 +56,22 @@ def none_if(expression_1: str, expression_2: str) -> (None | str):
 def async_measure_execution_time(async_func) :
     async def wrapper(*args, **kwargs):
         start_time = time.time()
-        await async_func(*args, **kwargs)
+        res = await async_func(*args, **kwargs)
         end_time = time.time()
         elapsed_time = end_time - start_time
         logger.info(f"{async_func.__name__} - elapsed time: {elapsed_time:.3f}s")
+        return res
 
     return wrapper
 
 def measure_execution_time(func) :
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        func(*args, **kwargs)
+        res = func(*args, **kwargs)
         end_time = time.time()
         elapsed_time = end_time - start_time
         logger.info(f"{func.__name__} - elapsed time: {elapsed_time:.3f}s")
+        return res
 
     return wrapper
 
@@ -51,18 +80,28 @@ async def fetch(session, url: str):
 
     async with semaphore:
         response = await session.get(url)
-        await response.html.arender(sleep = RENDER_SLEEP)
+        await response.html.arender(sleep = RENDER_SLEEP, retries=8)
 
-        logger.info(f"{url} : {response}")
+        logger.info(f"fetch: {url} response status code: {response.status_code}")
+        logger.info(f"response text: {response.text}")
 
         return response
 
-async def parse_code_elements_statuses(html :str) -> list[dict[str, str]]:
+def to_snake_case(s: str) -> str:
+  
+  s = unidecode(s) # remove accents
+  s = s.replace(',', '')
+
+  return '_'.join(
+    re.sub('([A-Z][a-z]+)', r' \1',
+    re.sub('([A-Z]+)', r' \1',
+    s.replace('-', ' '))).split()).lower()
+
+@measure_execution_time
+def parse_code_elements_statuses(html :str) -> list[dict[str, str]]:
 
     code_elements_statuses = []
-
     soup = BeautifulSoup(html, "html.parser")
-    grs_grid_legend_table_rows = soup.find('table', class_ = 'grs-grid-legend').find_all('tr')
 
     """
     Exemple in HTML
@@ -75,7 +114,9 @@ async def parse_code_elements_statuses(html :str) -> list[dict[str, str]]:
         <td align="left" width="95%">Exceptionally reserved code elements</td>
     </tr>
     """
-    
+
+    grs_grid_legend_table_rows = soup.find('table', class_ = 'grs-grid-legend').find_all('tr')
+
     for table_row in grs_grid_legend_table_rows:
         table_data = table_row.find_all('td')
 
@@ -95,7 +136,8 @@ async def parse_code_elements_statuses(html :str) -> list[dict[str, str]]:
 
     return code_elements_statuses
 
-async def parse_country_codes_collection(html :str) -> list[dict[str, str]]:
+@measure_execution_time
+def parse_country_codes_collection(html :str) -> list[dict[str, str]]:
 
     countries = []
     soup = BeautifulSoup(html, "html.parser")
@@ -119,7 +161,7 @@ async def parse_country_codes_collection(html :str) -> list[dict[str, str]]:
             td_anchor_href = td_anchor.get('href')
             td_anchor_text = td_anchor.get_text()
 
-        #print(f"""class: {td_class}, title: {td_title}, td_anchor: {td_anchor}, anchor_href: {td_anchor_href}, anchor_text: {td_anchor_text}, td_text: {td_text}""")
+        #logger.info(f"""class: {td_class}, title: {td_title}, td_anchor: {td_anchor}, anchor_href: {td_anchor_href}, anchor_text: {td_anchor_text}, td_text: {td_text}""")
         
         countries.append(
             {
@@ -132,61 +174,66 @@ async def parse_country_codes_collection(html :str) -> list[dict[str, str]]:
 
     return countries
 
-async def parse_summary(html :str) -> dict[str,str]:
+@measure_execution_time
+def parse_core_view_summary(html :str, language :str ='en') -> dict[str,str]:
+
+    if language not in ('en', 'fr'):
+        raise Exception('Unexpected language')
 
     soup = BeautifulSoup(html, "html.parser")
-
     summary = {}
+    
+    fr_to_en = {
+        "code_alpha_2" : "alpha_2_code",
+        "nom_court" : "short_name",
+        "forme_courte_du_nom_ecrite_en_minuscule" : "short_name_lower_case",
+        "nom_complet" : "full_name",
+        "code_alpha_3" : "alpha_3_code",
+        "code_numerique": "numeric_code",
+        "remarques" : "remarks",
+        "independant" : "independent",
+        "nom_de_territoire" : "territory_name",
+        "statut" : "status",
+        "remarque_concernant_le_statut" : "status_remark",
+        "remarque_partie_1" : "remark_part_1",
+        "remarque_partie_2" : "remark_part_2",
+        "remarque_partie_3" : "remark_part_3",
+        "code_alpha_4" : "alpha_4_code"
+    }
 
     core_view_lines = soup.find('div', 'core-view-summary').find_all('div', 'core-view-line')
 
     for line in core_view_lines:
-        #print(line)
         core_view_field_name_div = line.find('div', 'core-view-field-name')
         core_view_field_value_div = line.find('div', 'core-view-field-value') 
         field_name = None if core_view_field_name_div is None else core_view_field_name_div.get_text()
         field_value = None if core_view_field_value_div is None else core_view_field_value_div.get_text()
 
         if field_name is not None:
-            summary[field_name] = none_if(field_value,'')
+            normalised_field_name = to_snake_case(field_name)
+
+            if language == 'fr':
+                normalised_field_name = fr_to_en[normalised_field_name]
+
+            summary[normalised_field_name] = none_if(field_value,'')
 
     return summary
 
-def to_snake_case(s: str) -> str:
-  return '_'.join(
-    re.sub('([A-Z][a-z]+)', r' \1',
-    re.sub('([A-Z]+)', r' \1',
-    s.replace('-', ' '))).split()).lower()
-
-
-def to_csv(input:list[dict[str, str]], filename:str) -> None:
+def generate_csv(input:list[dict[str, str]], filename:str, expected_columns:list[str]) -> None:
 
     today = date.today()
     df = pd.DataFrame.from_dict(input)
-    df.rename(columns=lambda x: to_snake_case(x), inplace=True)
+    df = df[*expected_columns]
+    df.to_csv(f'./output/{today.strftime("%Y%m%d")}_{filename}.csv', sep='|', index=False, encoding='utf-8')
 
-    if filename == 'country-codes':
-        country_codes_columns = [
-            "alpha_2_code",
-            "alpha_3_code",
-            "alpha_4_code",
-            "numeric_code",
-            "short_name",
-            "short_name_lower_case",
-            "full_name",
-            "independent",
-            "territory_name",
-            "status",
-            "status_remark",
-            "remarks",
-            "remark_part_1",
-            "remark_part_2",
-            "remark_part_3"
-        ]
+def generate_country_codes_collection(country_codes_collection :list[dict[str, str]]):
+    generate_csv(country_codes_collection, 'country_codes_collection', COUNTRY_CODES_COLLECTION_EXCEPTED_COLUMNS)
 
-        df = df[country_codes_columns]
+def generate_code_elements_statuses(code_elements_statuses :list[dict[str, str]]):
+    generate_csv(code_elements_statuses, 'code_elements_statuses', CODE_ELEMENTS_STATUSES_EXPECTED_COLUMNS)
 
-    df.to_csv(f'./output/{today.strftime("%Y%m%d")}_{filename}.csv', sep='|', index=False)
+def generate_country_codes(parsed_countries :list[dict[str, str]], language :str ='en'):
+    generate_csv(parsed_countries, f'country-codes-{language}', COUNTRY_CODES_EXPECTED_COLUMNS)
 
 @async_measure_execution_time
 async def main() -> None:
@@ -198,33 +245,40 @@ async def main() -> None:
         html = response.html.html
         #logger.info(f"html : {html}")
         
-        code_elements_statuses = await parse_code_elements_statuses(html)
-        logger.info(f"code_elements_statuses : {code_elements_statuses}")
+        code_elements_statuses = parse_code_elements_statuses(html)
+        logger.info(f"{code_elements_statuses=}")
 
-        country_codes_collection = await parse_country_codes_collection(html)
-        logger.info(f"country_codes_collection[:5] : {country_codes_collection[:5]}")
+        country_codes_collection = parse_country_codes_collection(html)
+        logger.info(f"{country_codes_collection[:5]=}")
 
-        tasks = []
-        
-        for country in country_codes_collection:
+        english_urls_tasks = []
+        french_urls_tasks = []
+        #spanish_urls_tasks = []
+        #russian_urls_tasks = []
+
+        for country in country_codes_collection[:20]:
             page_id = country.get("page_id")
             
             if page_id is not None:
-                tasks.append(fetch(session, f"{BASE_URL}{page_id}"))
+                english_urls_tasks.append(fetch(session, f"{BASE_URL}{page_id}"))
+                french_urls_tasks.append(fetch(session, f"{BASE_URL}fr/{page_id}"))
+                #spanish_urls_tasks.append(fetch(session, f"{BASE_URL}es/{page_id}"))
+                #russian_urls_tasks.append(fetch(session, f"{BASE_URL}ru/{page_id}"))
 
-        logger.info(f"{len(tasks)} countries to parse")
+        logger.info(f"{len(english_urls_tasks)} countries to parse")
 
-        responses = await asyncio.gather(*tasks)
+        english_urls_responses = await asyncio.gather(*english_urls_tasks)
+        parsed_english_summaries = [parse_core_view_summary(r.html.html) for r in english_urls_responses]
+        logger.info(f"{parsed_english_summaries[:5]=}")
 
-        summaries :list = []
-        for response in responses:
-            summaries.append(await parse_summary(response.html.html))
+        french_urls_responses = await asyncio.gather(*french_urls_tasks)
+        parsed_french_summaries = [parse_core_view_summary(r.html.html, 'fr') for r in french_urls_responses]
+        logger.info(f"{parsed_french_summaries[:5]=}")
 
-        logger.info(f"summaries[:5] : {summaries[:5]}")
-
-        to_csv(code_elements_statuses, 'code-elements-statuses')
-        to_csv(country_codes_collection, 'country-codes-collection')
-        to_csv(summaries, 'country-codes')
+        generate_code_elements_statuses(code_elements_statuses)
+        generate_country_codes_collection(country_codes_collection)
+        generate_country_codes(parsed_english_summaries, 'en')
+        generate_country_codes(parsed_french_summaries, 'fr')
 
     except Exception as e:
         logger.error(e)
@@ -236,12 +290,9 @@ if __name__ == '__main__':
 
     asyncio.run(main())
 
-
     
     # Additional information
-    # Excepted fields : Administrative language(s) alpha-2 | Administrative language(s) alpha-3 | Local short name
-
-
+    # Expected fields : Administrative language(s) alpha-2 | Administrative language(s) alpha-3 | Local short name
     # Subdivisions
     # Excepted fields : Subdivision category | 3166-2 code | Subdivision name | Local variant | Language code | Romanization system | Parent subdivision
     # Change history of country code
